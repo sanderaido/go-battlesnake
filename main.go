07edd9e2799f5d6280ca14e96d2f523d57ad0cd9
Sander A <aido@tlu.ee>
2019-12-03 12:37:49 +0200

    Set up simple server
    runs on port 8080 and responds static messages

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+type server struct{}
 
 func main() {
-	fmt.Println("hello battlesnake")
+	battlesnakeServer := &server{}
+	http.Handle("/", battlesnakeServer)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func (battlesnakeServer *server) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write([]byte(`{"message": "hello battlesnake"}`))
 }
