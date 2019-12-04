87a728a3dccecd1a5afb72cea14bdec26aaf16cc
Sander A <aido@tlu.ee>
2019-12-04 13:26:13 +0200

    Wrote test for Index router

---
+++ src/battlesnake/routes_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	t.Run("returns basic welcome message", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		Index(response, request)
+
+		got := response.Body.String()
+		want := "This is a Battlesnake server"
+
+		if got != want {
+			t.Errorf("wanted %q, got %q", want, got)
+		}
+	})
+}
